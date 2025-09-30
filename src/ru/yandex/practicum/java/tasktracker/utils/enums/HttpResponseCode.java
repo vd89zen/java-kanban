208b2ab0ee2a0f5d5bd53ae0974fb7e7d0a562a6
Anton Antonov <vd89@yandex.ru>
2025-09-30 15:39:40 +1100

    code style

--- src/ru/yandex/practicum/java/tasktracker/utils/enums/HttpResponseCode.java
+++ src/ru/yandex/practicum/java/tasktracker/utils/enums/HttpResponseCode.java
@@ -1,18 +1,20 @@
 package ru.yandex.practicum.java.tasktracker.utils.enums;
 
 public enum HttpResponseCode {
-    OK (200),
-    CREATED (201),
-    BAD_REQUEST (400),
-    NOT_FOUND (404),
-    NOT_ACCEPTABLE (406),
-    INTERNAL_SERVER_ERROR (500);
+    OK(200),
+    CREATED(201),
+    BAD_REQUEST(400),
+    NOT_FOUND(404),
+    NOT_ACCEPTABLE(406),
+    INTERNAL_SERVER_ERROR(500);
 
     private final int code;
-    HttpResponseCode(int code){
+
+    HttpResponseCode(int code) {
         this.code = code;
     }
-    public int getCode(){
+
+    public int getCode() {
         return code;
     }
 }
